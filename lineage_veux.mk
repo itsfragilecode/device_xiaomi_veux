e05294b6aac1b56fdd432604075e248fc53043de
spezifisch <spezifisch+android@gmail.com>
2024-08-22 18:30:37 +0000

    extracted product info from running stock rom
    Change-Id: I00c11eeb7e4623b30e9b58b493e38c9c9ff8a874

--- lineage_veux.mk
+++ lineage_veux.mk
@@ -17,23 +17,22 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# Google
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Product
 PRODUCT_NAME := lineage_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_SYSTEM_NAME := veux_global
 
-# PixelOS variant
-#PRODUCT_BRAND := Redmi
-#PRODUCT_MODEL := 2201116SG
-# CherishOS variant
+# Branding: POCO X4 Pro 5G
+PRODUCT_SYSTEM_NAME := veux_p_global
 PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO X4 Pro 5G
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_MODEL := 2201116PG
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="veux_global-user 13 TKQ1.221114.001 V14.0.6.0.TKCMIXM release-keys" \
+    PRIVATE_BUILD_DESC="veux_p_global-user 13 TKQ1.221114.001 V14.0.6.0.TKCMIXM release-keys" \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := Redmi/veux_global/veux:13/TKQ1.221114.001/V14.0.6.0.TKCMIXM:user/release-keys
+BUILD_FINGERPRINT := POCO/veux_p_global/veux:13/TKQ1.221114.001/V14.0.6.0.TKCMIXM:user/release-keys
 
